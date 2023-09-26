ac090ff3bd951cbe56f7d48a42a92894d5a49555
yatin yadav <49709956+yatinrao16@users.noreply.github.com>
2023-09-27 01:41:25 +0530

    Update Alert.js

--- src/modules/Alert.js
+++ src/modules/Alert.js
@@ -3,19 +3,12 @@ import React from 'react'
 
 function Alert(props) {
 
-    // const capitalised=(word)=>{
-    //     if(word==="danger"){
-    //         word= "error"
-    //     }
-    //     let cap = word.charAt(0).toUpperCase();
-    //     return cap + word.slice(1);
-    // }
     
       return (
-        <div style={{height:"2rem",zIndex:"101"}} > 
+        <div style={{height:"4rem",zIndex:"1000"}} > 
         {props.alert && 
-         <div className={`alert alert-${props.alert.type} `} role="alert" style={{height:"4rem",top:"3rem",width:"16rem",left:"35rem"}}>
-            <div className='mt-3 '> <strong>{" "}<i class="fa fa-check"></i></strong> {props.alert.msg}</div>
+         <div className={`alert alert-${props.alert.type} `} role="alert" style={{height:"3rem",top:"4rem",width:"16rem",left:"35rem"}}>
+            <div className= ''> <strong>{" "}<i class="fa fa-check"></i></strong> {props.alert.msg}</div>
           </div>}
           </div>
         
